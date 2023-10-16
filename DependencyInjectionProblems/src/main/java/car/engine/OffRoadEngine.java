2f6de346d2c4ae40d57dc8ddb04271e5a91b33a3
Michael Ndula <michaelndula@gmail.com>
2023-10-16 05:37:24 +0300

    Dependency Injection With Qualifier annotation

--- DependencyInjectionProblems/src/main/java/car/engine/OffRoadEngine.java
+++ DependencyInjectionProblems/src/main/java/car/engine/OffRoadEngine.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OffRoadEngine implements Engine{
-    private final int hp = 600;
+    private final int hp = 500;
 
     @Override
     public int getHp() {
